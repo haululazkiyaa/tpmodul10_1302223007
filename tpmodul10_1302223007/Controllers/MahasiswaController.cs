3a7778528cc33a39e01775e2fbe43e0900eefc14
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-04-29 00:02:39 +0700

    menambahkan API mahasiswa

---
+++ tpmodul10_1302223007/Controllers/MahasiswaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace tpmodul10_1302223007.Controllers
+{
+    [Route("api/[controller]")]
+    public class MahasiswaController : Controller
+    {
+        private static List<Mahasiswa> mhs = new List<Mahasiswa>
+        {
+            new Mahasiswa("Muhammad Haulul Azkiyaa", "1302223007"),
+            new Mahasiswa("Fauzein Mulya Warman", "1302223127"),
+            new Mahasiswa("Iksan Oktav Risandy", "1302223042"),
+            new Mahasiswa("Muhammad Dias Adani", "1302223134"),
+            new Mahasiswa("Muhammad Nur Shodiq", "1302223054")
+        };
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mhs;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mhs[id];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Mahasiswa value)
+        {
+            mhs.Add(value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            mhs.RemoveAt(id);
+        }
+    }
+}
+
---
+++ tpmodul10_1302223007/Mahasiswa.cs
@@ -0,0 +1,15 @@
+﻿namespace tpmodul10_1302223007
+{
+	public class Mahasiswa
+	{
+        public string nama { set; get; }
+        public string nim { set; get; }
+
+        public Mahasiswa(string nama, string nim)
+        {
+            this.nama = nama;
+            this.nim = nim;
+        }
+    }
+}
+
